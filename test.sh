027922510b8e31c8f1c998a98336669977ab31e2
Ending2015a <joe85812@yahoo.com.tw>
2017-10-22 22:43:07 +0800

    first commit

---
+++ test.sh
@@ -0,0 +1,1 @@
+python test_enet.py --checkpoint_dir=./log/train_original_ENet --logdir=./log/test_original_ENet
---
+++ train.sh
@@ -0,0 +1,1 @@
+python train_enet.py --weighting="ENET" --num_epochs=300 --logdir="./log/train_original_ENet"
